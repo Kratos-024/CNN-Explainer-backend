f10ca5405116fc1f9e685681ad4fc1f59585496d
Priyanshu Tiwari <Priyanshutiwari878@yahoo.com>
2026-01-15 16:57:19 +0530

    made allow_credentials False

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     # allow_origins=['http://localhost:5173'],
     allow_origins=['https://cnn-explainer-frontend.vercel.app'],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=['*'],
     allow_headers=['*'],
 )
